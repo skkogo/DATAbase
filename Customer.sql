0fff5d72529b1d3725c4d510ffbbc37e92116256
Saurabh_Kumar_Gond <sunnygond619@gmail.com>
2024-07-29 19:32:38 +0530

    Customer file is created and emplyoyee file is modified

---
+++ Customer.sql
@@ -0,0 +1,6 @@
+use college;
+select * from Student;
+update Student 
+set RollNumber= "4"
+where RollNumber ="5";
+set sql_safe_updates =0;
--- empoyee.sql
+++ empoyee.sql
@@ -1,14 +1,28 @@
-Create Database XYZComp;
-use  XYZComp;
-Create Table EmployeeInfo (
-id int Primary Key,
-name varchar(50),
-salary varchar (50)
+Create Database College;
+Use College;
+Create Table Student(
+ RollNumber int Primary key,
+ Name varchar (100),
+ Marks int  not null,
+ Grade varchar(10),
+ city varchar(50)
 );
-insert into  EmployeeInfo
-(id,name,salary)
-values 
-(1,"Adam", "45000"),
-(2,"Bob","65000"),
-(3,"Cassy","50000");
-select* from EmployeeInfo;
+insert into Student
+(RollNumber, Name, Marks, Grade, city)
+values
+(001, "Robin", "90", "A", "Mumbai"),
+(003, "Sky", "99", "A", "Kolkata"),
+(004, "Vineeta", "94", "A", "Delhi"),
+(005, "Rohit", "60", "8", "Delhi");
+
+select distinct city from Student;
+select Name from Student where marks<80;
+Select Name from Student where city= "Delhi";
+select * from Student order by Marks desc
+limit 3;
+select max(Marks) from Student;
+select min(Marks) from Student;
+select avg(Marks) from Student;
+select count(Name) from Student;
+select city, count(name) from Student
+group by city;
